e9520c630dd51e4a795df03dbce62eacfa66231b
udhaykumarbala <udhayk97@gmail.com>
2026-03-28 06:43:06 +0100

    Move syllabus (Timeline) below FAQ section

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
       <ProgramStats />
       <ProgramOverview />
       <Benefits />
-      <Timeline />
       <Mentors />
       <FAQ />
+      <Timeline />
       <CTA />
       <Footer />
     </main>
